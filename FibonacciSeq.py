9b00b36fdf672a27e1239a394560f6a107e0e931
dandx2 <dandx2@yahoo.com>
2019-07-15 11:49:47 -0600

    Add files via upload

---
+++ FibonacciSeq.py
@@ -0,0 +1,21 @@
+﻿
+numbers = [0]
+
+if(numbers [-1] < 2):
+    print(sum(numbers))
+    a = numbers[-1]
+    numbers.append(a + 1)
+
+
+
+while (a < 1000):
+    a = numbers[-1]
+    b = numbers[-2]
+
+    numbers.append(a + b)
+
+    print(numbers)
+    print(sum(numbers))
+
+ 
+ 
